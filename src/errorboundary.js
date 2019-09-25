532748d72b0f685a16bb0d10cf8ced93db3580d9
vishnuortan <54902884+vishnuortan@users.noreply.github.com>
2019-09-25 18:21:29 +0530

    Add files via upload

---
+++ src/errorboundary.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDom from "react-dom";
+ 
+export default class ExportBoundary extends React.Component{
+	constructor(props){
+		super(props);
+		this.state={
+			hasError:false
+		}
+	}
+	
+	static getDerivedStateFromError(error) {
+			return { hasError: true }
+	}
+
+  componentDidCatch(error, errorInfo) {
+    console.log(error);
+	console.log(errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+          return <h1>Something went wrong...</h1>;
+    }
+
+    return this.props.children; 
+  }
+}
